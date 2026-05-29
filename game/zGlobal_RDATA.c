b1822844785a7636e79087d09f06c80e154ebb5b
Abdulrazaq Alhendi <dev@aalhendi.com>
2026-05-29 13:37:02 +0300

    fix(data): restore level metadata debug names

--- game/zGlobal_RDATA.c
+++ game/zGlobal_RDATA.c
@@ -25,5 +25,21 @@ struct rData rdata = {
     .s_tiny = "tiny",
     .s_cortex = "cortex",
     .s_crash = "crash",
+
+    .s_screen18 = "screen18",
+    .s_credit20 = "credit20",
+    .s_credit19 = "credit19",
+    .s_credit18 = "credit18",
+    .s_credit17 = "credit17",
+    .s_credit16 = "credit16",
+    .s_credit15 = "credit15",
+    .s_credit14 = "credit14",
+    .s_credit13 = "credit13",
+    .s_credit12 = "credit12",
+    .s_credit11 = "credit11",
+    .s_credit10 = "credit10",
+    .s_screen15 = "screen15",
+    .s_asphalt2 = "asphalt2",
+    .s_asphalt1 = "asphalt1",
 };
 #endif
