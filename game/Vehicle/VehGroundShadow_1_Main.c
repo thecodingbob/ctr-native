5f4750c78df271715b4f348c6c1c2c0f58e2bcda
Abdulrazaq Alhendi <dev@aalhendi.com>
2026-06-09 14:32:43 +0300

    fix(vehicle): match retail ground shadow point 4 projection

--- game/Vehicle/VehGroundShadow_1_Main.c
+++ game/Vehicle/VehGroundShadow_1_Main.c
@@ -185,9 +185,9 @@ static void VehGroundShadow_BuildProjectionPoints(struct VehGroundShadowEntry *e
 	points[3][1] = (s16)((baseY + local[0][1]) - local[1][1]);
 	points[3][2] = (s16)((baseZ + local[0][2]) - local[1][2]);
 
-	points[4][0] = (s16)(baseX + local[1][0]);
-	points[4][1] = (s16)(baseY + local[1][1]);
-	points[4][2] = (s16)(baseZ + local[1][2]);
+	points[4][0] = (s16)(baseX + local[0][0]);
+	points[4][1] = (s16)(baseY + local[0][1]);
+	points[4][2] = (s16)(baseZ + local[0][2]);
 
 	points[5][0] = (s16)(baseX + local[2][0] + local[0][0]);
 	points[5][1] = (s16)(baseY + local[2][1] + local[0][1]);
